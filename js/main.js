6e9e6ff01f372fe36c46a0f58074863a6ae786bb
muhammadsodiq07 <muhammadodiqkoziyev1@gmail.com>
2022-04-06 21:22:53 +0500

    aadded

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ let elForm = document.querySelector(".form");
 let elInput = document.querySelector(".input");
 let elButton = document.querySelector(".btn");
 let div = document.querySelector(".div");
+let main = document.querySelector(".mainDiv").style.display = 'none';
+let load = document.querySelector(".load").style.display = 'block'
+
+
+
+setTimeout(() => {
+  document.querySelector(".load").style.display = 'none';
+  document.querySelector(".mainDiv").style.display = 'block';
+},4000)
 
 elButton.addEventListener("click", () => {
   let span = document.createElement("span");
@@ -22,3 +31,5 @@ elButton.addEventListener("click", () => {
   }, 1000);
   elInput.value = "";
 });
+
+
